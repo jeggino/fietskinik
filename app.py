2941f61725fc479d139273cb7ece48ab83b52e3c
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2022-12-19 18:46:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,14 +9,11 @@ import altair as alt
 # Connect to Deta Base with your Project Key
 deta = Deta(st.secrets["deta_key"])
 
-# Create a new database "example-db"
+# Create a new database
 db = deta.Base("project_fietsklinik")
-# # Define the drive to store the files.
-# drive = deta.Drive("project_2_drive_1")
-
 # -------------- FUNCTIONS --------------
 
-def insert_period(date,time_shift, name, e_mail, buurt,werkzaamheedeb, materiaal, opmerking):
+def insert_period(date, time_shift, name, e_mail, buurt, werkzaamheedeb, materiaal, opmerking):
     """Returns the user on a successful user creation, otherwise raises and error"""
     return db.put({"date": date, "time_shift": time_shift, "name": name, "e_mail": e_mail, "buurt": buurt, 
                    "werkzaamheedeb": werkzaamheedeb, "materiaal":materiaal, "opmerking":opmerking})
@@ -68,72 +65,6 @@ if selected == "Make an appointment":
         "---"
         submitted = st.form_submit_button("Save Data")
         if submitted:
-            insert_period(date,time_shift, name, e_mail, buurt,werkzaamheedeb, materiaal, opmerking)
+            insert_period(date, time_shift, name, e_mail, buurt, werkzaamheedeb, materiaal, opmerking)
             st.success("You booked an appointment!")
             
-# # --- PLOT PERIODS ---
-# if selected == "Data Visualization":
-#      with st.form("plot_form"):
-    
-#         # ---FILTER THE DATASET---
-#         db_content = db.fetch().items
-#         df = pd.DataFrame(db_content)
-#         filter_multiselect = st.multiselect("Select a species", df["species"].unique().tolist(), default=df["species"].unique().tolist())
-
-#         df_2 = df[df.species.isin(filter_multiselect)][["lat", "lon", "date", "species", "n_specimens", "comment"]]
-
-
-#         # ---CREATE TABS ---
-#         tab1, tab2 = st.tabs(["🗃 Data","📈 Chart & 🗺️ Map"])
-
-#         with tab1:
-#             # ---show dataframe---
-#             st.dataframe(df_2,use_container_width=True)    
-
-#             # ---CREATE A DOWNLOAD BOTTON ---
-#             csv = df_2.to_csv().encode('utf-8')
-#             st.download_button(
-#                 label="Download data as CSV",
-#                 data=csv,
-#                 file_name='df.csv',
-#                 mime='text/csv',
-#             )
-
-
-#         with tab2:
-#         # ---CREATE THE PLOT---
-#             source = df_2.groupby("species", as_index=False)['n_specimens'].sum()
-#             plot_ = alt.Chart(source).mark_bar().encode(y='species', x='n_specimens',
-#                                                     tooltip=['species', 'n_specimens'])
-#             st.altair_chart(plot_, use_container_width=True)
-
-#             # ---CREATE A DOWNLOAD BOTTON ---
-#             fig = plot_.save("fig.html")     
-#             with open("fig.html", "rb") as file:
-#                 btn = st.download_button(
-#                         label="Download image",
-#                         data=file,
-#                         file_name="fig.html",
-#                       )
-
-#             left, right = st.columns(2)
-
-#             with left:
-#                 m = folium.Map(location=[df_2["lat"].mean(),df_2["lon"].mean()])
-#                 for row, columns in df_2.iterrows():
-#                     folium.Marker(location=[columns["lat"],columns["lon"]]).add_to(m)
-#                 output = st_folium(m)
-#                 output
-
-
-#             with right:
-#                 img_name = df_2[(df_2.lat==output["last_object_clicked"]["lat"]) & (df_2.lon==output["last_object_clicked"]["lng"])]["image_name"].values[0]
-#                 caption = df_2[(df_2.lat==output["last_object_clicked"]["lat"]) & (df_2.lon==output["last_object_clicked"]["lng"])]["comment"].values[0]
-#                 img = drive.get(img_name).read()
-#                 st.image(img, caption=caption)
-        
-        
-     
-
-
-         
