c4b27e8f0283cc08940831b8a82506871bb21a60
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2022-12-20 09:33:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 
 # --- NAVIGATION MENU ---
 selected = option_menu(
-    menu_title="Fietskliniek",
+    menu_title=None,
     options=["Make an appointment", "Cancel an appointment"],
     icons=["bi-journal-check", "bi-x-octagon-fill"],  # https://icons.getbootstrap.com/
     orientation="horizontal",
