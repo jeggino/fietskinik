93ecfe5ac900c0cc9465df2c0d078f212e26ac9b
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-09 17:15:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,8 @@ with left:
     st.image(image)
     
 with right:
-    st.markdown("Fietskliniek is a wonderful place where you can learn how to repair your own bike supervised by us just for 5 euros! we make our tools and space available for only 5 euros, but, as we :red[MUST] pay the rent every month, we ask to pay in advance at this :blue[_link_]. If you cancel the appointment we will give you the money back without a problem. The space is available only on Tuesday and Thursday, and three shifts are possible. We ask you about your experiences and also some other information that you will find in the form...")
+    st.markdown("Fietskliniek is a wonderful place where you can repair your own bike for just 5 euros! we will help you, and make our tools and space available, but, as we :red[MUST] pay the rent every month. For this reason, we ask you to pay in advance at this :blue[_link_]. If you cancel the appointment we will give you the money back.") 
+    st.markdown("At moment, the space is available only on Tuesday and Thursday, and three shifts are possible. We wish ask you about your experiences and also some other information that you will find in the form...see you soon!")
 
 
 # --- INPUT & SAVE PERIODS ---
