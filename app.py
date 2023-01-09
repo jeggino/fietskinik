5bdfb2c39d9d1323ff00c42639adccac226c8f4f
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-09 16:34:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -78,8 +78,6 @@ if selected == "Make an appointment":
         name = st.text_input("Name*", placeholder="Enter your name here ...")
         e_mail = st.text_input("E-mail*", placeholder="Enter your e-mail here ...")
         number = st.text_input("Telophone number*", placeholder="Enter your number here ...")
-        if type(number) != int:
-            st.error('Telephone number incorrect', icon="🚨")
         buurt = st.selectbox("Buurt", buurt_choice)
         expertise = st.selectbox("What is your expertise?", expertise_choice )
         werkzaamheedeb = st.text_input("", placeholder="Werkzaamheden ...")
@@ -97,7 +95,9 @@ if selected == "Make an appointment":
         # submit the data
         submitted = st.form_submit_button("Save Data")
         if submitted:
-            if name and e_mail and number:
+            if type(number) != int:
+                st.error('Telephone number incorrect', icon="💥")
+            elif name and e_mail and number:
                 day = parser.parse(date).strftime("%A")
                 if day == "Thursday" or day == "Tuesday":
 
