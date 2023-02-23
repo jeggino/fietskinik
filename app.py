b27a3686e2f78e24dabc4fe67e06cf1cb4fe288b
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-02-23 18:43:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ if selected == "Make an appointment":
     if membership == "I have a Membership":
         membership_number = st.text_input("", placeholder="Membership number to fill in ...",label_visibility="collapsed")
     elif membership == "I will use it only Once (€10 per 2 hours)":
-        "https://www.ing.nl/particulier/betaalverzoek/index.html?trxid=WZvack2Jq3lg7dM97EFKhnNUzvFLgZPI"
+        "https://www.ing.nl/particulier/betaalverzoek/index.html?trxid=4xGXt7LA0Kaipfn9tEllAPl7AcrbDhjR"
         
     with st.form("entry_form", clear_on_submit=False):                  
         date = st.date_input("Date (only Tuesday or Thursday)")
