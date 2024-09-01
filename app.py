528b737292ae7b3937be856403c67e254d55187a
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-09-01 17:31:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,16 @@ def fun(dict_, date):
 #_________vakantie_______
 
 
+# HORIZONTAL_RED = "images/horizontal_red.png"
+# ICON_RED = "images/icon_red.png"
+# HORIZONTAL_BLUE = "images/horizontal_blue.png"
+# ICON_BLUE = "images/icon_blue.png"
+
+# options = [HORIZONTAL_RED, ICON_RED, HORIZONTAL_BLUE, ICON_BLUE]
+# sidebar_logo = st.selectbox("Sidebar logo", options, 0)
+# main_body_logo = st.selectbox("Main body logo", options, 1)
+
+st.logo("https://cdn4.iconfinder.com/data/icons/general13/png/256/world.png", "https://cdn4.iconfinder.com/data/icons/general13/png/256/world.png")
 # --- SETTINGS ---
 page_title = None
 page_icon = " :bike: "  # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
